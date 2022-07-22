d7f85821e32be48e1f6c3bcfd2194717e249f712
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-07-22 08:26:08 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,49 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+TEST_CASE("get the temperature limits for each cooling type") {
+  temperatureLimitMap tempRangeList=getTemperatureRangeList();
+  REQUIRE(tempRangeList.size() == 3);
+  REQUIRE(tempRangeList.begin()->first == PASSIVE_COOLING);
+  REQUIRE(tempRangeList[PASSIVE_COOLING].first == 0);
+  REQUIRE(tempRangeList[PASSIVE_COOLING].second == 35);
+}
+
+TEST_CASE("classify the given temperature based on given ranges") {
+  temperatureLimitMap tempRangeList=getTemperatureRangeList();
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,-10,tempRangeList) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,70,tempRangeList) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,20,tempRangeList) == NORMAL);
+}
+
+TEST_CASE("get temperature status message list") {
+  temperatureStatusList tempStatusList=getTemperatureStatusMessageList();
+  REQUIRE(tempStatusList.size() == 3);
+  REQUIRE(tempStatusList.begin()->first == NORMAL);
+  REQUIRE(tempStatusList.begin()->second == "Temperature is normal");
+}
+
+TEST_CASE("Retrieve the message sent from controller") {
+  temperatureStatusList tempStatusList=getTemperatureStatusMessageList();
+	std::string expectedMessage="feed : 2";
+  REQUIRE(sendToController(TOO_HIGH) ==expectedMessage);
+	printOnConsole(sendToController(TOO_HIGH));
+}
+
+TEST_CASE("Retrieve the message sent from email") {
+  temperatureStatusList tempStatusList=getTemperatureStatusMessageList();
+	std::string recepient = "a.b@c.com";
+  std::string status="Temperature is too high";
+	std::string expectedMessage="To: " + recepient + "\n" + "Hi," + status;
+  REQUIRE(sendToEmail(TOO_HIGH,tempStatusList) ==expectedMessage);
+}
+
+TEST_CASE("Test temperature checker and alert functionality") {
+  BatteryCharacter batteryType;
+  batteryType.coolingType = PASSIVE_COOLING;
+  checkAndAlert(TO_EMAIL, batteryType, 50);
 }
